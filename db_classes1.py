b94d3c06f0970c943166a26c600f60a31b0a4fb9
Taras Khaburskyi <ktaras88@gmail.com>
2022-08-11 14:08:11 +0200

    db_classes1

--- db_classes1.py
+++ db_classes1.py
@@ -5,7 +5,7 @@ import datetime
 class Database:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(f'{db_name}.db')
-        self.c = self.conn.cursor()
+        self.cursor = self.conn.cursor()
 
     def save(self, sql_script):
         self.execute_sql(sql_script)
@@ -17,12 +17,12 @@ class Database:
         return result
 
     def execute_sql(self, sql_script):
-        return self.c.execute(f'''{sql_script}''')
+        return self.cursor.execute(f'''{sql_script}''')
 
 
 class Department:
-    def __init__(self, ID, name, abbreviation, max_amount, database):
-        self.ID = ID
+    def __init__(self, id, name, abbreviation, max_amount, database):
+        self.id = id
         self.name = name
         self.abbreviation = abbreviation
         self.max_amount = max_amount
@@ -31,26 +31,17 @@ class Department:
 
     def save(self):
         self.sql_script = f'''INSERT INTO DEPARTMENT (ID, name, abbreviation, max_amount) 
-            VALUES ('{self.ID}', '{self.name}', '{self.abbreviation}', '{self.max_amount}')'''
+            VALUES ('{self.id}', '{self.name}', '{self.abbreviation}', '{self.max_amount}')'''
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
         self.sql_script = f'''SELECT {args} FROM department'''
         return self.database.get(self.sql_script)
 
-    # def check_amount_employee_department(self, department_id):
-    #     pass
-
-    def get_salary(self, employee_id):
-        experience = self.employment_date - self.fired_date
-        if experience > 0:
-            return self.rate*experience*1.012
-        return self.rate
-
 
 class Person:
-    def __init__(self, ID, first_name, second_name, middle_name, passport, birthday, place_birthday, address, database):
-        self.ID = ID
+    def __init__(self, id, first_name, second_name, middle_name, passport, birthday, place_birthday, address, database):
+        self.id = id
         self.first_name = first_name
         self.second_name = second_name
         self.middle_name = middle_name
@@ -64,18 +55,18 @@ class Person:
     def save(self):
         self.sql_script = f'''INSERT INTO DEPARTMENT (ID, first_name, second_name, middle_name, passport, birthday, 
             place_birthday, address) 
-            VALUES ('{self.ID}', '{self.first_name}', '{self.second_name}', '{self.middle_name}', '{self.passport}', 
+            VALUES ('{self.id}', '{self.first_name}', '{self.second_name}', '{self.middle_name}', '{self.passport}', 
             '{self.birthday}', '{self.place_birthday}', '{self.address}')'''
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
-        self.sql_script = f'''SELECT {args} FROM department'''
+        self.sql_script = f'''SELECT {args} FROM person'''
         return self.database.get(self.sql_script)
 
 
 class Position:
-    def __init__(self, ID, position, rate, vacation_days, database):
-        self.ID = ID
+    def __init__(self, id, position, rate, vacation_days, database):
+        self.id = id
         self.position = position
         self.rate = rate
         self.vacation_days = vacation_days
@@ -84,17 +75,17 @@ class Position:
 
     def save(self):
         self.sql_script = f'''INSERT INTO DEPARTMENT (ID, position, rate, vacation_days) 
-            VALUES ('{self.ID}', '{self.position}', '{self.rate}', '{self.vacation_days}')'''
+            VALUES ('{self.id}', '{self.position}', '{self.rate}', '{self.vacation_days}')'''
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
-        self.sql_script = f'''SELECT {args} FROM department'''
+        self.sql_script = f'''SELECT {args} FROM position'''
         return self.database.get(self.sql_script)
 
 
 class Employee:
-    def __init__(self, ID, employment_date, fired_date, head_officer, person_id, position_id, database):
-        self.ID = ID
+    def __init__(self, id, employment_date, fired_date, head_officer, person_id, position_id, database):
+        self.ID = id
         self.employment_date = employment_date
         self.fired_date = fired_date
         self.head_officer = head_officer
@@ -106,12 +97,12 @@ class Employee:
     def save(self):
         self.sql_script = f'''INSERT INTO DEPARTMENT (ID, employment_date, fired_date, head_officer, 
             person_id, person_id) 
-            VALUES ('{self.ID}', '{self.employment_date}', '{self.fired_date}', '{self.head_officer}', 
+            VALUES ('{self.id}', '{self.employment_date}', '{self.fired_date}', '{self.head_officer}', 
             '{self.person_id}', '{self.position_id}')'''
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
-        self.sql_script = f'''SELECT {args} FROM department'''
+        self.sql_script = f'''SELECT {args} FROM employee'''
         return self.database.get(self.sql_script)
 
 
@@ -131,13 +122,13 @@ class DepartmentEmployee:
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
-        self.sql_script = f'''SELECT {args} FROM department'''
+        self.sql_script = f'''SELECT {args} FROM DEPARTMENT_EMPLOYEE'''
         return self.database.get(self.sql_script)
 
 
 class HistoryVacation:
-    def __init__(self, ID, start_date, finish_date, employee_id, database):
-        self.ID = ID
+    def __init__(self, id, start_date, finish_date, employee_id, database):
+        self.id = id
         self.start_date = start_date
         self.finish_date = finish_date
         self.employee_id = employee_id
@@ -146,15 +137,41 @@ class HistoryVacation:
 
     def save(self):
         self.sql_script = f'''INSERT INTO DEPARTMENT (ID, start_date, finish_date, employee_id) 
-            VALUES ('{self.ID}', '{self.start_date}', '{self.finish_date}', '{self.employee_id}')'''
+            VALUES ('{self.id}', '{self.start_date}', '{self.finish_date}', '{self.employee_id}')'''
+        employees_on_vacation = f'''?????????????'''
+        if employees_on_vacation >= 5:
+            return print('Currently on vacation are 5 employees')
         return self.database.save(self.sql_script)
 
     def get(self, *args, **kwargs):
-        self.sql_script = f'''SELECT {args} FROM department'''
+        self.sql_script = f'''SELECT {args} FROM HistoryVacation'''
         return self.database.get(self.sql_script)
 
-    def check_number_employees_vacation(self, department_id):
-        pass
+
+class Company:
+    def __init__(self, employee, position):
+        self.employee = employee
+        self.position = position
+
+    def get_salary(self):
+        experience = self.employee.employment_date - self.employee.fired_date
+        if experience > 0:
+            return self.position.rate*experience*1.012
+        return self.position.rate
 
 
-db = Database('factory')
+# class Person:
+#     pass
+#
+# class Company:
+#         class Database:
+#         class Department:
+#         class Employee:
+#         class DepartmentEmployee:
+#         class Position:
+#         class HistoryVacation:
+
+        # def __init__(self, employee, position):
+        #     self.employee = employee # (екземпляр класу Person)
+        #     self.position = position # (екземпляр класу Position)
+        # def get_salary(self):
